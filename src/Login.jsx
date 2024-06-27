7da0dd951a20e8a14633c16c282396494d8c7b8c
AnibalSV <anibalsequera2024@gmail.com>
2024-06-27 14:40:47 +0200

    Directorios restaurados

---
+++ src/Login.jsx
@@ -0,0 +1,48 @@
+import react from "react";
+import "./Login.css";
+
+export const Login = () => {
+  return (
+    <div className="App">
+      <div className="formContainer">
+        <form>
+          <h2 className="tittle">LOGIN</h2>
+          <br />
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" className="input" />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="input"
+            />
+          </div>
+          <br />
+          <div className="below">
+            <span>
+              <label htmlFor="remember">
+                <input
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+                  className="checkbox"
+                />
+                Remember
+              </label>
+            </span>
+            <span>
+              <a href="#">¿Forgot password?</a>{" "}
+            </span>
+          </div>
+          <br />
+          <button type="submit">SUBMIT</button>
+        </form>
+      </div>
+    </div>
+  );
+};
---
+++ src/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Index} from './Index';
+import {Login} from './Login';
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+  <Index/>
+  </>
+);
