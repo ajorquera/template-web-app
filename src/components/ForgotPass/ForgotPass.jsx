72dd2de8bf0541213a8f25668decfa4fc90ecf30
AnibalSV <anibalsequera2024@gmail.com>
2024-07-20 14:14:42 +0200

    Maquetación y estilos de primer formulario para recuperar contraseña

--- src/components/ForgotPass/ForgotPass.jsx
+++ src/components/ForgotPass/ForgotPass.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './ForgotPass.css'
 
 export const ForgotPass = () => {
     return (
-        <h1>RECUPERAR CONTRASEÑA</h1>
+        <div className="App">
+            <div className="formContainer">
+                <form action="" className="form">
+                    <h2 className="formTitle">FORGOT PASSWORD?</h2>
+                    <div className="formDiv">
+                        <p className='formText'>Please enter your email to confirm your identify.</p>
+                    </div>
+                    <div className="formDiv">
+                        <label htmlFor="" className="formLabel">
+                            Email 
+                            <input type="email" name="" id="" className="formInput" />
+                        </label>
+                    </div>
+                    <div className="formBtn formDiv ">
+                        <button type="submit" className='formBtnSubmit'>SUBMIT</button>
+                    </div>
+                    <div className="formDiv logIn">
+                        <Link to='/App/'className='formLink'>Log in</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
