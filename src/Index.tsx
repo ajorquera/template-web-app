8a231de29a7ef6b6a031c3ea63f934aa7b160552
andres <jorquera.ad@gmail.com>
2024-09-23 14:58:20 +0300

    dev

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-Amplify.configure({});
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
