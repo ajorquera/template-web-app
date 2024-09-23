6707f3c04f8a534a1af079aabf5fdd0a0a7c97ff
Andres Jorquera <ajorquera@users.noreply.github.com>
2024-09-23 22:52:12 +0300

    feat: added amplify backend (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
-import { Profile } from "./components/Profile/Profile";
-import { Login } from "./pages/Login/Login";
-
-import "./pages/Login/Login.css";
-import { auth } from "./FirebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
-import router from "./router";
 import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import React from "react";
 
 const App = () => {
-  const [usuario, setUsuario] = useState(null);
-
-  onAuthStateChanged(auth, (usuarioFirebase) => {
-    if (usuarioFirebase) {
-      setUsuario(usuarioFirebase);
-    } else {
-      setUsuario(null);
-    }
-  });
-  {usuario ? <Profile correoUsuario={usuario.email} /> : <Login />}
-
   return (
     <React.StrictMode>
       <RouterProvider router={router} />
---
+++ src/pages/Login/components/AmplifyLogin.tsx
@@ -0,0 +1,16 @@
+import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import React from 'react';
+
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
